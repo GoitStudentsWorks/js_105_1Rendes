54b008bcd1296ec90a7571ce2c3476540ccdaa31
1Rendes <1admin@ukr.net>
2024-07-06 19:06:15 +0200

    hero-fixed

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,9 +2,6 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.accordion-container', {
-  duration: 100,
+  duration: 250,
   showMultiple: true,
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
 });
