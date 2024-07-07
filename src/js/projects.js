b1ba5502545ff40d8e7b89fdf7b07ad1ef70dda3
Yurii <y.tsyhykal@gmail.com>
2024-07-07 14:02:43 +0200

    added arrows

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,9 +1,26 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 import 'swiper/css';
-
-const swiper = new Swiper('.swiper', {
+const swiperProjects = new Swiper('.project-swiper', {
   direction: 'horizontal',
-  loop: true,
-  speed: 400,
-  spaceBetween: 100,
+  speed: 1500,
+  centerInsufficientSlides: true,
+  centeredSlidesBounds: true,
+  // effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
+
+const prevBtnProjects = document.querySelector('.projects-prev-box');
+const nextBtnProjects = document.querySelector('.projects-next-box');
+
+prevBtnProjects.addEventListener('click', () => {
+})
+
+
+
