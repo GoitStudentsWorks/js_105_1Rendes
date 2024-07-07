5e34df32686ae74298c6ffee7f1c40757420b85e
Yurii <y.tsyhykal@gmail.com>
2024-07-07 17:13:34 +0200

    added js to swipper, settings to swipper, export function for disabled/enabled buttons, styles for disabled buttons

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,26 +1,56 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css';
+
+const prevBtnProjects = document.querySelector('.projects-prev-btn');
+const nextBtnProjects = document.querySelector('.projects-next-btn');
+
 const swiperProjects = new Swiper('.project-swiper', {
   direction: 'horizontal',
   speed: 1500,
+  spaceBetween: 30,
   centerInsufficientSlides: true,
   centeredSlidesBounds: true,
+  grabCursor: true,
+  preventClicks: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
+  on: {
+    slideChange: function () {
+      getDisabledOrEnabledButtons(this, prevBtnProjects, nextBtnProjects);
+    },
+  },
+  // allowSlidePrev: true,
+  // allowSlideNext,
   // effect: 'coverflow',
-  coverflowEffect: {
-    rotate: 30,
-    slideShadows: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // coverflowEffect: {
+  //   rotate: 30,
+  //   slideShadows: true,
+  // },
 });
 
-const prevBtnProjects = document.querySelector('.projects-prev-box');
-const nextBtnProjects = document.querySelector('.projects-next-box');
+if (swiperProjects.isBeginning) {
+  prevBtnProjects.disabled = true;
+}
+
+export function getDisabledOrEnabledButtons(t, prev, next) {
+  if (t.isBeginning) {
+    prev.disabled = true;
+  } else if (t.isEnd) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+    prev.disabled = false;
+  }
+}
 
 prevBtnProjects.addEventListener('click', () => {
-})
-
-
-
+  swiperProjects.slidePrev();
+});
+nextBtnProjects.addEventListener('click', () => {
+  swiperProjects.slideNext();
+});
