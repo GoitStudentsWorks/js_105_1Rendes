4c07823ba6b02f71c6c6483a9ea6b538e5f77ec5
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-07-08 18:30:41 +0300

    work-toogether-js-post

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,32 +1,76 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import Axios from 'axios';
 
 const axios = Axios.create({
   baseURL: 'https://portfolio-js.b.goit.study/api',
 });
 
-const footerButon = document.querySelector('.footer-form-button');
 const footerForm = document.querySelector('.footer-form');
 const inputEmail = document.querySelector('.footer-form-email');
 const inputComment = document.querySelector('.footer-form-comment');
+const footerModal = document.querySelector('.footer-modal');
+const footerModalCloseBtn = document.querySelector(
+  '.footer-modal-close-btn-icon'
+);
 
-let userEmail = ``;
-let userComment = ``;
+// const inputElem = document.getElementById('input');
+
+// inputElem.addEventListener('input', () => {
+//   const maxLengthInput = inputElem.length;
+//   console.log(maxLengthInput);
+//   const currentLength = inputElem.value.length;
+//   console.log(currentLength);
+
+//   if (currentLength > maxLengthInput) {
+//     inputElem.value = inputElem.value.substring(maxLengthInput);
+//   }
+// });
 
 async function createPost(email, comment) {
-  const params = new URLSearchParams({
-    email: email,
-    comment: comment,
-  });
-
-  const res = await axios.post({ params });
-  return res.data;
+  try {
+    const res = await axios.post('/requests', { email, comment });
+    return res.data;
+  } catch (error) {
+    throw new Error(error.res.data.message);
+  }
 }
 
 footerForm.addEventListener(`submit`, async e => {
-  e.preventDefault;
+  e.preventDefault();
 
-  userEmail = inputEmail.value.trim();
-  userComment = inputComment.value.trim();
+  const userEmail = inputEmail.value.trim();
+  const userComment = inputComment.value.trim();
 
-  const data = await createPost(userEmail, userComment);
+  try {
+    const data = await createPost(userEmail, userComment);
+    if (data) {
+      showModal();
+      hideModal();
+    }
+    footerForm.reset();
+  } catch (error) {
+    iziToast.warning({
+      title: 'WARNING',
+      message: 'Enter the correct data',
+      position: 'bottomCenter',
+    });
+    console.error('Помилка:', error);
+  }
+});
+
+function showModal() {
+  footerModal.classList.add('is-open');
+}
+
+function hideModal() {
+  footerModalCloseBtn.addEventListener('click', () => {
+    footerModal.classList.remove('is-open');
+  });
+}
+
+window.addEventListener('click', e => {
+  if (e.target == footerModal) {
+    footerModal.classList.remove('is-open');
+  }
 });
