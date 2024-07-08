204ff7db4d7120b8b2508ce2eaca3ca89a039bc5
1Rendes <1admin@ukr.net>
2024-07-08 16:59:37 +0200

    faq-done

--- src/js/faq.js
+++ src/js/faq.js
@@ -9,14 +9,14 @@
 //   },
 // });
 
-import Accordion from 'accordion-js'; 
-import 'accordion-js/dist/accordion.min.css'; 
- 
-const open = document.querySelector('.ac-trigger'); 
- 
-new Accordion(['.container-first', '.container-second'], { 
-  duration: 250, 
-  showMultiple: true, 
-}); 
- 
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+const open = document.querySelector('.ac-trigger');
+
+new Accordion(['.container-first', '.container-second'], {
+  duration: 250,
+  showMultiple: true,
+});
+
 open.click();
