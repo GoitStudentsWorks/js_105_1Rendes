ea2865185a6751cfd7ca423ebb6c041608f47c75
Volodymyr Prodchenko <vladimir.prodchenko@gmail.com>
2024-07-08 15:45:08 +0300

    add faq sections html+css+js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,10 +1,22 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container', {
-  duration: 100,
-  showMultiple: true,
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
-});
+// new Accordion('.accordion-container', {
+//   duration: 100,
+//   showMultiple: true,
+//   onOpen: function (currentElement) {
+//     console.log(currentElement);
+//   },
+// });
+
+import Accordion from 'accordion-js'; 
+import 'accordion-js/dist/accordion.min.css'; 
+ 
+const open = document.querySelector('.ac-trigger'); 
+ 
+new Accordion(['.container-first', '.container-second'], { 
+  duration: 250, 
+  showMultiple: true, 
+}); 
+ 
+open.click();
