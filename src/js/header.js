a7b2d81880b5eb7f9fb8bcd103ee42ee2bbbb809
1Rendes <1admin@ukr.net>
2024-07-09 14:32:40 +0200

    anchors except benefits

--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,13 @@ const mobMenuBtn = document.querySelector('.header-mob-menubtn');
 const headerMenu = document.querySelector('.header-menu');
 const headerMobMenu = document.querySelector('.header-mob-menu');
 const headerCloseIcon = document.querySelector('.header-close-icon');
+const headerMobButtons = document.querySelector('.header-mobmenu-list');
+
+headerMobButtons.addEventListener('click', e => {
+  if (e.target.nodeName === 'A') {
+    headerMobMenu.classList.toggle('header-mob-hidden');
+  }
+});
 
 menuBtn.addEventListener('click', () =>
   headerMenu.classList.toggle('header-hidden')
