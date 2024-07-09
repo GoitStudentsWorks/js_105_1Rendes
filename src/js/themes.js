bd622f09d1d33da9fa9f119d9e288e7720b9b8cc
1Rendes <1admin@ukr.net>
2024-07-10 00:29:11 +0200

    themes fix

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,55 +1,56 @@
 const colorsWebPage = {
   green: {
-    '--bg-image-default': 'url(/img/hero/Hero.png)',
+    '--bg-image-default': 'url(../img/hero/Hero.png)',
     '--text-color-default': 'rgba(198, 227, 39, 1)',
     '--bg-color-default': '#c6e327',
     '--bg-color-hover-default': '#b2d61a',
-    '--bg-image-tablet': 'url(/img/hero/hero-tablet-greens.png)',
-    '--bg-image-mobile': 'url(/img/hero/hero-mobile-green.png)',
+    '--bg-image-tablet': 'url(../img/hero/hero-tablet-greens.png)',
+    '--bg-image-mobile': 'url(../img/hero/hero-mobile-green.png)',
   },
   red: {
-    '--bg-image-default': 'url(/img/hero/hero-desktop-background.png)',
+    '--bg-image-default': 'url(../img/hero/hero-desktop-background.png)',
     '--text-color-default': 'rgba(237, 59, 68)',
     '--bg-color-default': '#ed3b44',
     '--bg-color-hover-default': '#e0373f',
-    '--bg-image-tablet': 'url(/img/hero/hero-tablet-backround.png)',
-    '--bg-image-mobile': 'url(/img/hero/hero-mob-background.png)',
+    '--bg-image-tablet': 'url(../img/hero/hero-tablet-backround.png)',
+    '--bg-image-mobile': 'url(../img/hero/hero-mob-background.png)',
   },
   blue: {
-    '--bg-image-default': 'url(/img/hero/hero-desktop-blue.png)',
+    '--bg-image-default': 'url(../img/hero/hero-desktop-blue.png)',
     '--text-color-default': 'rgba(0, 65, 232, 1)',
     '--bg-color-default': '#0041e8',
     '--bg-color-hover-default': '#003bcf',
-    '--bg-image-tablet': 'url(/img/hero/hero-tablet-blues.png)',
-    '--bg-image-mobile': 'url(/img/hero//hero-mobile-blue.png)',
+    '--bg-image-tablet': 'url(../img/hero/hero-tablet-blues.png)',
+    '--bg-image-mobile': 'url(../img/hero//hero-mobile-blue.png)',
   },
   coral: {
-    '--bg-image-default': 'url(/img/hero/hero-desktop-coral.png)',
+    '--bg-image-default': 'url(../img/hero/hero-desktop-coral.png)',
     '--text-color-default': 'rgba(230, 83, 60, 1)',
     '--bg-color-default': '#e6533c',
     '--bg-color-hover-default': '#e64933',
-    '--bg-image-tablet': 'url(/img/hero/hero-tablet-corals.png)',
-    '--bg-image-mobile': 'url(/img/hero/hero-mobile-coral.png)',
+    '--bg-image-tablet': 'url(../img/hero/hero-tablet-corals.png)',
+    '--bg-image-mobile': 'url(../img/hero/hero-mobile-coral.png)',
   },
   slate: {
-    '--bg-image-default': 'url(/img/hero/hero-desktop-slate.png)',
+    '--bg-image-default': 'url(../img/hero/hero-desktop-slate.png)',
     '--text-color-default': 'rgba(43, 68, 65, 1)',
     '--bg-color-default': '#2b4441',
     '--bg-color-hover-default': '#293f3c',
-    '--bg-image-tablet': 'url(/img/hero/hero-tablet-slates.png)',
-    '--bg-image-mobile': 'url(/img/hero/hero-mobile-slate.png)',
+    '--bg-image-tablet': 'url(../img/hero/hero-tablet-slates.png)',
+    '--bg-image-mobile': 'url(../img/hero/hero-mobile-slate.png)',
   },
   orange: {
-    '--bg-image-default': 'url(/img/hero/hero-desktop-orange.png)',
+    '--bg-image-default': 'url(../img/hero/hero-desktop-orange.png)',
     '--text-color-default': 'rgba(255, 127, 8, 1)',
     '--bg-color-default': '#ff7f08',
     '--bg-color-hover-default': '#ff7b08',
-    '--bg-image-tablet': 'url(/img/hero/hero-tablet-oranges.png)',
-    '--bg-image-mobile': 'url(/img/hero/hero-mobile-orange.png)',
+    '--bg-image-tablet': 'url(../img/hero/hero-tablet-oranges.png)',
+    '--bg-image-mobile': 'url(../img/hero/hero-mobile-orange.png)',
   },
 };
 
-document.querySelector('.navigation-colors')
+document
+  .querySelector('.navigation-colors')
   .addEventListener('click', function (e) {
     if (e.target.classList.contains('box-colors')) {
       const color = e.target.getAttribute('data-color');
